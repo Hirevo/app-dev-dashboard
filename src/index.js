b7af66b19db86fd5fe39abcac302d2aafc76d0d3
Nicolas Polomack <nipolom@live.fr>
2018-10-01 13:46:00 +0200

    Added base server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("It works !");
+const express = require("express");
+const { config } = require("./config");
+const { createServer } = require("http");
+
+const app = express();
+const server = createServer(app);
+
+app.get(/^\/(?:index(?:.html)?)?\/?$/, async (req, res) => {
+    res.end("Hello, World !");
+});
+
+server.listen(config.application.port, config.application.address);
---
+++ src/utils.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+const base_path = path.resolve(path.join(__dirname, ".."));
+
+module.exports = {
+    base_path
+};
