d3a660ecf98bfed65a3049ed1656c3f3f74da89a
Arzad <cedric.thomas@epitech.eu>
2018-10-10 14:24:05 +0200

    [~] config

--- server/src/index.js
+++ server/src/index.js
@@ -1,32 +1,32 @@
-const pg = require("pg");
+// const pg = require("pg");
 
-const pool = new pg.Pool({
-    host: process.env.PG_HOST ||"localhost",
-    port: process.env.PG_PORT || 5432,
-    user: process.env.PG_USER || "dashboard",
-    password: process.env.PG_PASSWD || "ji9QqN50nNZwCBlmoWPJ8GBbAQDanxPlW4NkWjlmm+OSpovnEIK0w4d+tp72wE3J",
-    database: process.env.PG_DB || "dashboard"
-});
+// const pool = new pg.Pool({
+//     host: process.env.PG_HOST ||"localhost",
+//     port: process.env.PG_PORT || 5432,
+//     user: process.env.PG_USER || "dashboard",
+//     password: process.env.PG_PASSWD || "ji9QqN50nNZwCBlmoWPJ8GBbAQDanxPlW4NkWjlmm+OSpovnEIK0w4d+tp72wE3J",
+//     database: process.env.PG_DB || "dashboard"
+// });
 
-function conn_db(pool) {
-    return pool.connect();
-}
+// function conn_db(pool) {
+//     return pool.connect();
+// }
 
-function query_db(conn, query, values = []) {
-    return conn.query(query, values).then(result => result.rows);
-}
+// function query_db(conn, query, values = []) {
+//     return conn.query(query, values).then(result => result.rows);
+// }
 
-const conn = conn_db(pool);
-//query_db(conn, "SELECT * FROM ")
+// const conn = conn_db(pool);
+// //query_db(conn, "SELECT * FROM ")
 
 
-// const express = require("express");
-// const { config } = require("./config");
-// const { createServer } = require("http");
+const express = require("express");
+const { config } = require("./config");
+const { createServer } = require("http");
 
-// const app = express();
-// const server = createServer(app);
+const app = express();
+const server = createServer(app);
 
-// app.use(express.static(config.application.public_folder))
-// server.listen(config.application.port, config.application.address);
+app.use(express.static(config.application.public_folder))
+server.listen(config.application.port, config.application.address);
 
