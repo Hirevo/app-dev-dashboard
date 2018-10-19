4a96db7e9d2734571ae621b642bf2a1f3825436a
Cédric Thomas <cedric.thomas@epitech.eu>
2018-10-19 19:27:12 +0200

    [~} db + steam real widget

---
+++ src/client/widgets/steam-game-watcher.js
@@ -0,0 +1,67 @@
+import { until } from "../lit-html/directives/until.js";
+import { html, render } from "../lit-html/lit-html.js";
+
+export class SteamGameWatcher extends HTMLElement {
+    static get tag() { return "steam-game-watcher"; }
+    get tag() { return SteamGameWatcher.tag; }
+    get widget_id() { return this._id; }
+
+    get template() {
+        return html`
+        <div style="padding: 10px">
+            ${this.render_body()}
+        </div>`;
+    }
+
+    constructor(id, { timer, game }) {
+        super();
+        this._id = id;
+        this.timer = timer;
+        this.game = game;
+        this.render();
+    }
+
+    render() {
+        render(this.template, this);
+        setTimeout(this.render.bind(this), this.timer);
+    }
+
+    render_body() {
+        const loader = html`<img src="/static/loading.gif" width="128" height="128" alt="Loading...">`;
+        return html`${until(this.fetch_data(), loader)}`;
+    }
+
+    error_print(reason) {
+        return html`
+        <h1 style="height: 100px; display: flex; align-items: center; justify-content: center; font-weight: bold;">
+            Error: ${reason}
+        </h1>`;
+    }
+
+    async fetch_data() {
+        try {
+            const resp = await fetch(`/api/steam/games/${this.game}`, { credentials: "same-origin" });
+            const { type, ...rest } = await resp.json();
+            if (type == "error")
+                throw rest.reason;
+            const game = rest.payload.game;
+            const players = rest.payload.playing;
+            return html`
+            <div class="content" style="display: flex; flex-direction: column; align-items: center; justify-content: center">
+                <h4 style="text-align: center">${this.game}</h4>
+                <img src=" http://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_logo_url}.jpg">
+                <div style="display: flex">
+                    <ul>
+                        ${players.map(elem => html`${elem.name}`)}
+                    </ul>
+                </div>
+            </div>`;
+        } catch (reason) {
+            return this.error_print(reason);
+        }
+    }
+}
+
+window.customElements.define(SteamGameWatcher.tag, SteamGameWatcher);
+
+export default SteamGameWatcher;
--- src/server/api/steam.mjs
+++ src/server/api/steam.mjs
@@ -6,35 +6,52 @@ import { config } from "../config.mjs";
 
 export const router = express.Router({ caseSensitive: true });
 
+async function getFriends(api_key, token) {
+    const friends_resp = await fetch(`http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=${api_key}&steamid=${token}&format=json`);
+    if (friends_resp.ok == false)
+        throw {code: 500, message: "Bad response."};
+    const friends_json = await friends_resp.json();
+    const friends = friends_json.friendslist.friends;
+    const ids = friends.map(elem => elem.steamid).join(",");
+    const summaries_resp = await fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${api_key}&steamids=${ids}&format=json`);
+    if (summaries_resp.ok == false)
+        throw {code: 500, message: "Bad response."};
+    const payload = await summaries_resp.json();
+    const data = payload.response.players.map(({ personaname, steamid, personastate, gameid }) => ({steamid: steamid, name: personaname, state: personastate, game: gameid}));
+    for (let elem of data) {
+        if (elem.game) {
+            const game_resp = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${api_key}&steamid=${elem.steamid}&include_appinfo=1&include_played_free_games=1&format=json`);
+            if (game_resp.ok == false)
+                throw {code: 500, message: "Bad response."};
+            const game_json = await game_resp.json();
+            const { name } = game_json.response.games.find(game => game.appid == elem.game) || {};
+            elem.game = name;
+        }
+    }
+    return data;
+}
+
+async function getGames(api_key, token) {
+    const game_resp = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${api_key}&steamid=${token}&include_appinfo=1&include_played_free_games=1&format=json`);
+    if (game_resp.ok == false)
+            throw {code: 500, message: "Bad response."};
+    const game_json = await game_resp.json();
+    const data = game_json.response.games.map(({name, playtime_forever, img_logo_url, img_icon_url, appid}) => ({name, playtime_forever, img_icon_url, img_logo_url, appid}));
+    return data;
+}
+
 router.get('/friends', authenticated_steam, async (req, resp) => {
     const conn = await conn_db(pool);
     try {
         const { api_key } = config.auth.steam;
         const [{ token }] = await query_db(conn, "select token from users join steam on users.id = steam.user_id where users.id = ?;", [req.user.id]);
-        const friends_resp = await fetch(`http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=${api_key}&steamid=${token}&format=json`);
-        if (resp.ok == false)
-            throw "Bad response.";
-        const friends_json = await friends_resp.json();
-        const friends = friends_json.friendslist.friends;
-        const ids = friends.map(elem => elem.steamid).join(",");
-        const summaries_resp = await fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${api_key}&steamids=${ids}&format=json`);
-        if (resp.ok == false)
-            throw "Bad response.";
-        const payload = await summaries_resp.json();
-        const data = payload.response.players.map(({ personaname, steamid, personastate, gameid }) => ({steamid: steamid, name: personaname, state: personastate, game: gameid}));
-        for (let elem of data) {
-            if (elem.game) {
-                const game_resp = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${api_key}&steamid=${elem.steamid}&include_appinfo=1&include_played_free_games=1&format=json`);
-                if (resp.ok == false)
-                    throw "Bad response.";
-                const game_json = await game_resp.json();
-                const { name } = game_json.response.games.find(game => game.appid == elem.game) || {};
-                elem.game = name;
-            }
-        }
+        const data = await getFriends(api_key, token);
         resp.json({payload: data});
     } catch (reason) {
-        resp.status(500).json({ type: "error", reason });
+        if (reason.code)
+            resp.status(reason.code).json({ type: "error", reason: reason.message });
+        else
+            resp.status(500).json({ type: "error", reason });
     } finally {
         conn.release();
     }
@@ -45,14 +62,38 @@ router.get('/games', authenticated_steam, async (req, resp) => {
     try {
         const { api_key } = config.auth.steam;
         const [{ token }] = await query_db(conn, "select token from users join steam on users.id = steam.user_id where users.id = ?;", [req.user.id]);
-        const game_resp = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${api_key}&steamid=${token}&include_appinfo=1&include_played_free_games=1&format=json`);
-        if (game_resp.ok == false)
-                throw "Bad response.";
-        const game_json = await game_resp.json();
-        const data = game_json.response.games.map(({name, playtime_forever, img_icon_url, appid}) => ({name, playtime_forever, img_icon_url, appid}));
+        const data = await getGames(api_key, token);
         resp.json({payload: data});
     } catch (reason) {
-        resp.status(500).json({ type: "error", reason });
+        if (reason.code)
+            resp.status(reason.code).json({ type: "error", reason: reason.message });
+        else
+            resp.status(500).json({ type: "error", reason });
+    } finally {
+        conn.release();
+    }
+});
+
+router.get(/^\/games\/(.+)\/?/, authenticated_steam, async (req, resp) => {
+    const game_name = req.params[0];
+    const conn = await conn_db(pool);
+    try {
+        const { api_key } = config.auth.steam;
+        const [{ token }] = await query_db(conn, "select token from users join steam on users.id = steam.user_id where users.id = ?;", [req.user.id]);
+        const games = await getGames(api_key, token);
+        const friends = await getFriends(api_key, token);
+        const game = games.find((game) => {return game.name == game_name});
+        if (!game)
+            throw {code: 404, message: "Game not found."};
+        const playing = friends.filter((friend) => {
+            return friend.game === game_name;
+        });
+        resp.json({payload: {game, playing}});
+    } catch (reason) {
+        if (reason.code)
+            resp.status(reason.code).json({ type: "error", reason: reason.message });
+        else
+            resp.status(500).json({ type: "error", reason });
     } finally {
         conn.release();
     }
