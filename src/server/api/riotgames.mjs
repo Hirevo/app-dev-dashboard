4b8ef4d9a0ebf6a822986902c56a333f0b5324b4
Cédric Thomas <cedric.thomas@epitech.eu>
2018-10-20 11:34:06 +0200

    [+] riotgales api

--- src/server/api/riotgames.mjs
+++ src/server/api/riotgames.mjs
@@ -27,16 +27,22 @@ router.get(/^\/in-game\/([^/]+)\/([^/]+)\/?/, async (req, res) => {
     }
 });
 
-router.get(/^\/forecast\/([A-Za-z]+)\/?/, async (req, res) => {
-    const city = req.params[0];
+router.get(/^\/rank\/([^/]+)\/([^/]+)\/?/, async (req, res) => {
+    const region = req.params[0];
+    const summoner_name = req.params[1];
 
     try {
-        const { api_key } = config.auth.weather;
-        const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&mode=json&units=metric&appid=${api_key}`;
-        const resp = await fetch(url);
-        if (resp.ok == false)
-            throw "Bad response.";
-        const payload = await resp.json();
+        const { api_key } = config.auth.riotgames;
+        const make_request = async (url) => {
+            const resp = await fetch(url, { headers: { "X-Riot-Token": api_key } });
+            if (resp.ok == false)
+                throw "Bad response.";
+            return await resp.json();
+        };
+        const url = `https://${region}.api.riotgames.com/lol/summoner/v3/summoners/by-name/${summoner_name}`;
+        const { id } = await make_request(url);
+        const url2 = `https://${region}.api.riotgames.com/lol/league/v3/positions/by-summoner/${id}`;
+        const payload = await make_request(url2);
         res.json({ type: "response", payload });
     } catch (reason) {
         res.status(404).json({ type: "error", reason });
